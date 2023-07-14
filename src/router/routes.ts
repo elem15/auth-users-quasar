5fa900ffc9d317661c1eb069b579e365da29ca6f
Elem <elem15@yandex.ru>
2023-07-14 17:57:07 +0300

    feat: add main layout

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,9 +6,17 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
   },
+  {
+    path: '/signin',
+    component: () => import('layouts/SignIn.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
+  {
+    path: '/signup',
+    component: () => import('layouts/SignUp.vue'),
+    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+  },
 
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
