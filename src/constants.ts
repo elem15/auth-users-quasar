3b4c040d3137534ae7dbf7013001760f0708bfa6
Elem <elem15@yandex.ru>
2024-01-28 23:29:07 +0300

    feat: new backend url

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,3 @@
 // export const HOST = 'http://localhost:3000'
-export const HOST = 'https://auth-users-baiv.onrender.com'
+// export const HOST = 'https://auth-users-baiv.onrender.com'
+export const HOST = 'https://nest-mern.koyeb.app'
